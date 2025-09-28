eadd6c9bd0bc54cd8b599b66742bb1f2357bfb2f
Sairam B Subramaniam <122884091+Sai-r-a-m@users.noreply.github.com>
2025-09-28 13:23:04 +0530

    Add files via upload

--- traffic_control.py
+++ traffic_control.py
@@ -12,7 +12,13 @@ lanes = ["N_C_0", "S_C_0", "E_C_0", "W_C_0", "C_N_0", "C_S_0", "C_E_0", "C_W_0"]
 TLS_ID = "C"
 
 vehicle_types = ["car", "truck", "bike"]
-routes = ["north_south", "south_north", "east_west", "west_east"]
+routes_dict = {
+    "north": ["N_left", "N_straight", "N_right"],
+    "south": ["S_left", "S_straight", "S_right"],
+    "east":  ["E_left", "E_straight", "E_right"],
+    "west":  ["W_left", "W_straight", "W_right"]
+}
+
 
 vehicle_counter = 0
 
@@ -43,14 +49,16 @@ def run():
     while step < 10000:
         traci.simulationStep()
 
-        if step % 10 == 0:
-            route = random.choice(routes)
+        if step % 1 == 0:
+            incoming = random.choice(["north", "south", "east", "west"])
+            route = random.choice(routes_dict[incoming])
             vtype = random.choice(vehicle_types)
             veh_id = f"veh{vehicle_counter}"
 
             if veh_id not in traci.vehicle.getIDList():
                 traci.vehicle.add(veh_id, routeID=route, typeID=vtype)
                 vehicle_counter += 1
+
 
         if step % phase_duration == 0:
             current_phase = (current_phase + 1) % 2
